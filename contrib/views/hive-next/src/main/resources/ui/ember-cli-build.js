d69fbf407ffd4575671aff3ef77126949a690513
Dipayan Bhowmick <dipayan.bhowmick@gmail.com>
2016-11-08 01:57:17 +0530

    Basic scaffold, fonts, color, wiring up of the UI

--- contrib/views/hive-next/src/main/resources/ui/ember-cli-build.js
+++ contrib/views/hive-next/src/main/resources/ui/ember-cli-build.js
@@ -5,6 +5,10 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    bootstrap: {
+      // List of Bootstrap plugins to use
+      plugins: []
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
